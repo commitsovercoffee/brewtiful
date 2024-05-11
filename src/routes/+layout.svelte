3c8ef4c42824abdf12e6e55a72ede3339f562f69
commitsovercoffee <commitsovercoffee@gmail.com>
2024-05-11 12:02:18 +0530

    adds fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import '../app.css';
+
+	// Supports weights 100-700
+	import '@fontsource/ibm-plex-serif';
+	// Supports weights 100-900
+	import '@fontsource-variable/inter';
 </script>
 
 <slot />
