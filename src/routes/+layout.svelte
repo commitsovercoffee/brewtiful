10d4f0f6f926268a3641fbc07ada3f5fe0d96bef
commitsovercoffee <commitsovercoffee@gmail.com>
2024-05-12 14:37:35 +0530

    adds page loader

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,15 +8,31 @@
 
 	import { fly } from 'svelte/transition';
 	import { cubicIn, cubicOut } from 'svelte/easing';
+
+	import { beforeNavigate, afterNavigate } from '$app/navigation';
+	import Loader from '$lib/components/Loader.svelte';
+
 	export let data;
+
+	let isLoading = false;
+	beforeNavigate(() => (isLoading = true));
+	afterNavigate(() => (isLoading = false));
 </script>
 
 <Nav />
+<!--
+A #key block takes an expression, and automatically re-renders its contents
+whenever that expression changes.
+-->
 {#key data.currentRoute}
 	<main
 		in:fly={{ easing: cubicOut, y: 10, duration: 300, delay: 400 }}
 		out:fly={{ easing: cubicIn, y: -10, duration: 300 }}
 	>
-		<slot />
+		{#if isLoading}
+			<Loader />
+		{:else}
+			<slot />
+		{/if}
 	</main>
 {/key}
