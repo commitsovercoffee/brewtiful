cbfe3a3d83e4c7e534fc8d85bda8a21250cce075
commitsovercoffee <commitsovercoffee@gmail.com>
2024-05-12 15:45:17 +0530

    adds heading links with rehype

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,9 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
 
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
@@ -14,7 +17,8 @@ const config = {
 	preprocess: [
 		vitePreprocess(),
 		mdsvex({
-			extensions: ['.md']
+			extensions: ['.md'],
+			rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings]
 		})
 	]
 };
